448497b1ad98862f5b9143f579ec885dcad43688
morine0122 <k0000.m000.job@gmail.com>
2025-03-06 01:50:15 +0900

    modify test rustdoc json

--- tests/rustdoc-json/impl-trait-precise-capturing.rs
+++ tests/rustdoc-json/impl-trait-precise-capturing.rs
@@ -1,3 +1,5 @@
+// ignore-tidy-linelength
+
 //@ is "$.index[*][?(@.name=='hello')].inner.function.sig.output.impl_trait[1].use[0].Lifetime" \"\'a\"
 //@ is "$.index[*][?(@.name=='hello')].inner.function.sig.output.impl_trait[1].use[1].Param" \"T\"
 //@ is "$.index[*][?(@.name=='hello')].inner.function.sig.output.impl_trait[1].use[2].Param" \"N\"
