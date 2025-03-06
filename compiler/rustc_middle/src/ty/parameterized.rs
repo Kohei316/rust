9d4be498be9fd0e03b0975eb173e2b34c1957c3a
morine0122 <k0000.m000.job@gmail.com>
2025-03-06 19:47:20 +0900

    refactor

--- compiler/rustc_middle/src/ty/parameterized.rs
+++ compiler/rustc_middle/src/ty/parameterized.rs
@@ -97,7 +97,7 @@ trivially_parameterized_over_tcx! {
     rustc_hir::def_id::DefIndex,
     rustc_hir::definitions::DefKey,
     rustc_hir::OpaqueTyOrigin<rustc_hir::def_id::DefId>,
-    rustc_hir::PreciseCapturingArgKind<Symbol,Symbol>,
+    rustc_hir::PreciseCapturingArgKind<Symbol, Symbol>,
     rustc_index::bit_set::DenseBitSet<u32>,
     rustc_index::bit_set::FiniteBitSet<u32>,
     rustc_session::cstore::ForeignModule,
